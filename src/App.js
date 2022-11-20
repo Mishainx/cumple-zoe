3d3b15761e1f35d0473f8d7a928c29375520073a
mishainx <fer.r@live.com.ar>
2022-11-20 06:49:19 -0300

    modificaciones

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
 import Panecillo from './components/panecillo/panecillo';
+import Title from './components/Title/title';
 
 function App() {
   return (
     <div className="App">
+      <Title/>
       <Panecillo/>
     </div>
   );
