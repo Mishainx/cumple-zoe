49a6c2e3742534dce4f10c20da66f217156966f5
mishainx <fer.r@live.com.ar>
2022-11-21 14:46:52 -0300

    modificaciones

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Panecillo from './components/panecillo/panecillo';
 import Title from './components/Title/title';
+import Recuadro from './components/recuadro/recuadro';
 
 function App() {
   return (
     <div className="App">
       <div className="CardContainer">
-      <Title/>
+
+      <Recuadro/>
       <Panecillo/>
       </div>
 
---
+++ src/components/recuadro/recuadro.js
@@ -0,0 +1,11 @@
+import '../recuadro/recuadro.scss'
+
+function Recuadro(){
+    return(
+        <div className='recuadro'>
+
+        </div>
+    )
+}
+
+export default Recuadro
