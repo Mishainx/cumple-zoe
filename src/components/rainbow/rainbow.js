83a45cde620bccad2685dc82bf0074274ec4c7f6
mishainx <fer.r@live.com.ar>
2022-11-21 16:39:15 -0300

    modificaciones

--- src/components/rainbow/rainbow.js
+++ src/components/rainbow/rainbow.js
@@ -2,12 +2,11 @@ import '../rainbow/rainbow.scss'
 
 function Rainbow(){
     return(
-        <>
+    <div className='rainbowContainer'>
     <img src='/img/rainbow.png' className='rainbow'/>
-    <div className='recuadro'></div>
     <h1 className='title'>Zoe</h1>
     <h2 className='subTitle'>Mi cumple!</h2>
-    </>
+    </div>
 
     )
 }
