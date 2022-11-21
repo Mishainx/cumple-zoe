fbb613b0c12b071ba3118144a6a8263e09d422b7
mishainx <fer.r@live.com.ar>
2022-11-21 20:37:08 -0300

    modificaciones

--- src/components/invitacion/invitacion.js
+++ src/components/invitacion/invitacion.js
@@ -3,6 +3,13 @@ import '../invitacion/invitacion.scss'
 function Invitacion(){
     return(
 <div className='Invitacion'>
+    <h2>Info</h2>
+    <div  className='infoInvitacion'>
+        <h3>Te espero para compartir juntos mis 5 años!</h3>
+        <p>Día: 30 de diciembre de 2022</p>
+        <p>Salón: Ciudad del Sol</p>
+        <p>Horario: 17:30 a 20hs.</p>
+    </div>
     
 </div>
 
