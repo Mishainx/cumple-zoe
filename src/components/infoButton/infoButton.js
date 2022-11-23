4b5bdca8d76ffa53d70b32a702e38614b9912ff2
mishainx <fer.r@live.com.ar>
2022-11-23 18:02:59 -0300

    modificaciones

--- src/components/infoButton/infoButton.js
+++ src/components/infoButton/infoButton.js
@@ -1,10 +1,18 @@
 import '../infoButton/infoButton.scss'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 
 function InfoButton(){
+    useEffect(()=>{
+        AOS.init({duration:1500, delay:1000})
+    },[])
+
+
     return(
-        <>
-    <button className='infoButton'>Ver invitación!</button>
-    </>
+        <div> 
+    <button className='infoButton' data-aos="fade-up">Ver invitación!</button>
+    </div>
 
     )
 }
